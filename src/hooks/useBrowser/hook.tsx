9476d7b3d234ed6df7445f241afb48b7231d6565
Milton Ivan (WhiL) García López <dev.ivagarcia@gmail.com>
2023-12-02 19:30:22 -0600

    update status browser

--- src/hooks/useBrowser/hook.tsx
+++ src/hooks/useBrowser/hook.tsx
@@ -1,15 +1,23 @@
+import { mountAtom } from "@/components/atoms/AtomModal";
 import isBrave from "@/utils/browser/browser";
+import { useAtomValue } from "jotai";
 import { useEffect } from "react";
 import { toast } from "sonner";
-
 const useBrowser = () => {
+  const browser = useAtomValue(mountAtom);
   useEffect(() => {
     if (isBrave()) {
-      toast.warning("Brave browser settings", {
-        description: "Change the settings to allow fingerpriting",
-      });
+      if (browser) {
+        toast.warning("Brave browser settings", {
+          description: "Change the settings to allow fingerpriting",
+        });
+      } else {
+        toast.message("Brave browser settings", {
+          description: "Change the settings to allow fingerpriting",
+        });
+      }
     }
-  }, []);
+  }, [browser]);
 };
 
 export default useBrowser;
