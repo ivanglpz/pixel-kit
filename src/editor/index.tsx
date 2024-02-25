9c786d4b6af70388a0bb3a0877294e5d9a0cb00f
Ivanglpz <miltonivan117@gmail.com>
2024-02-25 12:12:10 -0600

    update import main component

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -2,7 +2,8 @@ import PixelKitShapes from "./elements";
 import useBrowser from "./hooks/useBrowser/hook";
 import useStopZoom from "./hooks/useStopZoom/hook";
 import LayoutPixelEditor from "./layout";
-import PixelKitStage from "@/editor/stage/index";
+import PixelKitStage from "./stage/index";
+
 const PixelEditor = () => {
   useStopZoom();
   useBrowser();
