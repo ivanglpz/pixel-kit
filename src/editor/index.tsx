e079fb1425ba20733cd37a89f294d7ae6e7c9a9c
Ivanglpz <miltonivan117@gmail.com>
2024-02-25 10:57:18 -0600

    update folters and name hooks

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,8 +1,12 @@
 import PixelKitShapes from "./elements";
+import useBrowser from "./hooks/useBrowser/hook";
+import useStopZoom from "./hooks/useStopZoom/hook";
 import LayoutPixelEditor from "./layout";
 import PixelKitEditorStage from "./stage";
 
 const PixelEditor = () => {
+  useStopZoom();
+  useBrowser();
   return (
     <LayoutPixelEditor>
       <PixelKitEditorStage>
