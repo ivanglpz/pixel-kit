5888b77c36cc3ad6f4b81f6f67f8dcdaa3e1b275
Ivanglpz <miltonivan117@gmail.com>
2024-03-27 23:15:50 -0600

    validate image in image editor mode

--- src/editor/layout/left/image/index.tsx
+++ src/editor/layout/left/image/index.tsx
@@ -1,7 +1,9 @@
+import { Valid } from "@/components/valid";
 import { Button } from "@/editor/components/button";
 import { useImageRender } from "@/editor/hooks/image/hook";
 import { css } from "@stylespixelkit/css";
 import { ChangeEvent, useRef } from "react";
+import { createPortal } from "react-dom";
 
 export const ImageConfiguration = () => {
   const { img, handleSetImageRender } = useImageRender();
@@ -30,62 +32,148 @@ export const ImageConfiguration = () => {
     reader.readAsDataURL(file);
   };
   return (
-    <div
-      className={css({
-        padding: "lg",
-        display: "flex",
-        flexDirection: "column",
-        gap: "lg",
-        backgroundColor: "primary",
-        borderRadius: "lg",
-        border: "container",
-      })}
-    >
-      <p
+    <>
+      <Valid isValid={!Boolean(img?.base64?.length)}>
+        {createPortal(
+          <main
+            className={css({
+              position: "absolute",
+              top: 0,
+              zIndex: 9999,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0,0,0,0.5)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            })}
+          >
+            <div
+              className={css({
+                padding: "lg",
+                display: "flex",
+                flexDirection: "column",
+                gap: "lg",
+                backgroundColor: "primary",
+                borderRadius: "lg",
+                border: "container",
+                width: 200,
+                height: 130,
+              })}
+            >
+              <p
+                className={css({
+                  fontSize: "md",
+                  color: "text",
+                  fontWeight: "bold",
+                })}
+              >
+                Please enter a image
+              </p>
+              <p
+                className={css({
+                  fontSize: "sm",
+                  color: "text",
+                  fontWeight: "normal",
+                  opacity: 0.7,
+                })}
+              >
+                Only accept images...
+              </p>
+              <Button
+                text="Browser Files"
+                onClick={() => inputRef.current?.click()}
+              />
+              {img?.name ? (
+                <p
+                  className={css({
+                    fontSize: "x-small",
+                    color: "text",
+                    fontWeight: "normal",
+                    fontStyle: "italic",
+                    opacity: 0.7,
+                  })}
+                >
+                  {img?.name}
+                </p>
+              ) : null}
+              <input
+                ref={inputRef}
+                type="file"
+                color="white"
+                accept="image/*"
+                onChange={handleFiles}
+                className={css({
+                  backgroundColor: "red",
+                  width: 0,
+                  height: 0,
+                })}
+              />
+            </div>
+          </main>,
+          document.body
+        )}
+      </Valid>
+      <div
         className={css({
-          fontSize: "md",
-          color: "text",
-          fontWeight: "bold",
+          padding: "lg",
+          display: "flex",
+          flexDirection: "column",
+          gap: "lg",
+          backgroundColor: "primary",
+          borderRadius: "lg",
+          border: "container",
         })}
       >
-        Files
-      </p>
-      <p
-        className={css({
-          fontSize: "sm",
-          color: "text",
-          fontWeight: "normal",
-          opacity: 0.7,
-        })}
-      >
-        Only accept images...
-      </p>
-      <Button text="Browser Files" onClick={() => inputRef.current?.click()} />
-      {img?.name ? (
         <p
           className={css({
-            fontSize: "x-small",
+            fontSize: "md",
+            color: "text",
+            fontWeight: "bold",
+          })}
+        >
+          Files
+        </p>
+        <p
+          className={css({
+            fontSize: "sm",
             color: "text",
             fontWeight: "normal",
-            fontStyle: "italic",
             opacity: 0.7,
           })}
         >
-          {img?.name}
+          Only accept images...
         </p>
-      ) : null}
-      <input
-        ref={inputRef}
-        type="file"
-        color="white"
-        accept="image/*"
-        onChange={handleFiles}
-        className={css({
-          backgroundColor: "red",
-          width: 0,
-          height: 0,
-        })}
-      />
-    </div>
+        <Button
+          text="Browser Files"
+          onClick={() => inputRef.current?.click()}
+        />
+        {img?.name ? (
+          <p
+            className={css({
+              fontSize: "x-small",
+              color: "text",
+              fontWeight: "normal",
+              fontStyle: "italic",
+              opacity: 0.7,
+            })}
+          >
+            {img?.name}
+          </p>
+        ) : null}
+        <input
+          ref={inputRef}
+          type="file"
+          color="white"
+          accept="image/*"
+          onChange={handleFiles}
+          className={css({
+            backgroundColor: "red",
+            width: 0,
+            height: 0,
+          })}
+        />
+      </div>
+    </>
   );
 };
