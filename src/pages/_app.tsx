83c3d5dddc6ff5290f4b93c230e3446bc65f8bbd
Ivanglpz <miltonivan117@gmail.com>
2024-04-04 21:24:52 -0600

    remove images and update seo app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,12 @@ import type { AppPropsWithLayout } from "next/app";
 import { Toaster } from "sonner";
 import { css } from "../../styled-system/css";
 import { Analytics } from "@vercel/analytics/react";
-import SeoComponent from "@/components/seo";
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   return (
     <>
       <Analytics />
-      <SeoComponent
-        image="/home/editorimages.png"
-        title="Pixel Kit - v1"
-        content="pixels, kit, design, editor, react, nextjs"
-        description="Transform ideas into visual masterpieces with Pixel Kit, a potent open-source editing tool. Elevate your creative projects through intuitive design, advanced image editing."
-        url="https://pixel-kit.vercel.app/"
-      />
+
       <Toaster
         richColors
         expand={true}
