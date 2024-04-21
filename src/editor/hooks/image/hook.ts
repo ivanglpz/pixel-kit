7469405be5f1e3bce53bfd82c0bb7ad23a4905be
Ivanglpz <miltonivan117@gmail.com>
2024-04-21 13:05:25 -0600

    clip image editor mode

--- src/editor/hooks/image/hook.ts
+++ src/editor/hooks/image/hook.ts
@@ -10,21 +10,30 @@ const initialRenderImage = {
 };
 
 export const imageRenderAtom = atom(initialRenderImage);
-export const imageOriginalAtom = atom<typeof initialRenderImage | null>(
-  initialRenderImage
-);
+export const imageOriginalAtom = atom<typeof initialRenderImage | null>(null);
 
 export const useImageRender = () => {
   const [img, setImg] = useAtom(imageRenderAtom);
   const [originalImage, setOriginalImage] = useAtom(imageOriginalAtom);
+  console.log({ originalImage });
+
   const handleSetImageRender = (values: typeof initialRenderImage) => {
     setOriginalImage(values);
     setImg(values);
   };
+  const handleSetClipImage = (values: typeof initialRenderImage) => {
+    setImg(values);
+  };
 
+  const handleResetImage = () => {
+    if (originalImage) {
+      setImg(originalImage);
+    }
+  };
   return {
     img,
-    imageCliped: originalImage,
+    handleSetClipImage,
     handleSetImageRender,
+    handleResetImage,
   };
 };
