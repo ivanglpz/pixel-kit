218a7976bebf34647641cf398e428acc8d7e05e2
ivanglpz <miltonivan117@gmail.com>
2025-05-17 17:14:47 -0600

    update layout with grid main

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -6,6 +6,7 @@ import PxStage from "./stage";
 import { useConfiguration } from "./hooks/useConfiguration";
 import SidebarRight from "./layout/SidebarRight";
 import { css } from "@stylespixelkit/css";
+import { HeaderLogo } from "./layout/HeaderLogo";
 
 const PixelEditor = () => {
   useStopZoom();
@@ -17,20 +18,47 @@ const PixelEditor = () => {
     <div
       id="pixel-app"
       className={css({
-        display: "grid",
-        gridTemplateColumns: "240px 1fr 240px",
         backgroundColor: "black",
-        flex: 1,
         height: "100%",
         width: "100%",
+        display: "grid",
+        gridTemplateRows: "35px 1fr",
       })}
     >
-      <p>left</p>
+      <header
+        className={css({
+          padding: "md",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderBottom: "1px solid gray",
+        })}
+      >
+        <HeaderLogo />
+      </header>
+      <div
+        className={css({
+          display: "grid",
+          gridTemplateColumns: "240px 1fr 240px",
+          backgroundColor: "black",
+          flex: 1,
+          height: "100%",
+          width: "100%",
+        })}
+      >
+        <div
+          className={css({
+            borderRight: "1px solid gray",
+          })}
+        >
+          <p>left</p>
+        </div>
 
-      <PxStage>
-        <AllLayers />
-      </PxStage>
-      <SidebarRight />
+        <PxStage>
+          <AllLayers />
+        </PxStage>
+        <SidebarRight />
+      </div>
     </div>
   );
 };
