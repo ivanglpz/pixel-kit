15c76af6c2f7f17770cddfbd1f30b3da4509b779
ivanglpz <miltonivan117@gmail.com>
2025-05-18 14:33:02 -0600

    update sidebar nodes left with structure

--- src/editor/components/Nodes.tsx
+++ src/editor/components/Nodes.tsx
@@ -3,6 +3,8 @@ import { SHAPES_NODES } from "../states/shapes";
 import { css } from "@stylespixelkit/css";
 import { CHANGE_SHAPE_NODE_ATOM, NODE_ATOM } from "../states/nodes";
 import icons, { iconsWithTools } from "@/assets";
+import { useSelectedShape } from "../hooks";
+import { useState } from "react";
 
 export const Nodes = ({ item }: { item: SHAPES_NODES }) => {
   const value = useAtomValue(item.state);
@@ -11,18 +13,21 @@ export const Nodes = ({ item }: { item: SHAPES_NODES }) => {
 
   const SET_CHANGE = useSetAtom(CHANGE_SHAPE_NODE_ATOM);
   const handleDragStart = (e: React.DragEvent) => {
-    setDraggedNode(item); // guardamos el nodo que se está arrastrando
+    setDraggedNode(item);
   };
 
   const handleDragOver = (e: React.DragEvent) => {
-    e.preventDefault(); // permite soltar
+    e.preventDefault();
   };
 
   const handleDrop = (e: React.DragEvent) => {
     e.preventDefault();
-
     SET_CHANGE({ id: item.id });
   };
+
+  const { shapeSelected, handleSetShapeSelected } = useSelectedShape();
+
+  const [isExpanded, setIsExpanded] = useState(true); // ← toggle state
 
   return (
     <>
@@ -36,37 +41,106 @@ export const Nodes = ({ item }: { item: SHAPES_NODES }) => {
           width: "100%",
           color: "text",
           padding: "md",
-          borderRadius: "md",
+          borderTopRightRadius: "md",
+          borderBottomRightRadius: "md",
           fontSize: "sm",
           listStyle: "none",
-          display: "flex",
+          display: "grid",
+          gridTemplateColumns: "13px 13px 1fr 10px",
           flexDirection: "row",
           alignItems: "center",
+          backgroundColor: value.tool === "GROUP" ? "primary" : "transparent",
           gap: "lg",
           _hover: {
             backgroundColor: "primary",
           },
+          cursor: "pointer",
         })}
+        onClick={() => {
+          handleSetShapeSelected(value);
+        }}
       >
+        {value.tool === "GROUP" && childrens.length > 0 ? (
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsExpanded((prev) => !prev);
+            }}
+            className={css({
+              marginLeft: "auto",
+              backgroundColor: "transparent",
+              border: "none",
+              color: "white",
+              cursor: "pointer",
+              fontSize: "sm",
+            })}
+          >
+            {isExpanded ? (
+              <svg
+                width="20"
+                height="18"
+                viewBox="0 0 20 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M9.42653 12.3894L3.76953 6.73236L5.18353 5.31836L10.1335 10.2684L15.0835 5.31836L16.4975 6.73236L10.8405 12.3894C10.653 12.5768 10.3987 12.6821 10.1335 12.6821C9.86837 12.6821 9.61406 12.5768 9.42653 12.3894Z"
+                  fill="white"
+                />
+              </svg>
+            ) : (
+              <svg
+                width="19"
+                height="20"
+                viewBox="0 0 19 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M12.8894 10.7063L7.23236 16.3633L5.81836 14.9493L10.7684 9.99928L5.81836 5.04928L7.23236 3.63528L12.8894 9.29228C13.0768 9.47981 13.1821 9.73412 13.1821 9.99928C13.1821 10.2644 13.0768 10.5188 12.8894 10.7063Z"
+                  fill="white"
+                />
+              </svg>
+            )}
+
+            {/* {isExpanded ? "➖" : "➕"} */}
+          </button>
+        ) : (
+          <div></div>
+        )}
         {iconsWithTools[value.tool]}
 
         <p>{value.tool}</p>
+        <div
+          className={css({
+            backgroundGradient:
+              value.id === shapeSelected.id ? "primary" : "transparent",
+            width: "100%",
+            height: "10px",
+            borderRadius: "lg",
+          })}
+        ></div>
       </li>
 
-      {childrens?.length > 0 ? (
+      {childrens?.length > 0 && isExpanded && (
         <ul
           className={css({
-            paddingLeft: "lg",
+            marginLeft: "xlg",
             display: "flex",
             flexDirection: "column",
             gap: "md",
+            borderLeft: "1px solid #535353",
           })}
         >
           {childrens.map((e) => (
             <Nodes key={value.id + e.tool + e.id + "childrens"} item={e} />
           ))}
         </ul>
-      ) : null}
+      )}
     </>
   );
 };
