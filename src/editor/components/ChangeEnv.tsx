86fb1c9c5086a2687059b89557cb2b28819c2895
ivanglpz <miltonivan117@gmail.com>
2025-08-09 15:52:13 -0600

    update props with sidebar right shape config

--- src/editor/components/ChangeEnv.tsx
+++ src/editor/components/ChangeEnv.tsx
@@ -1,16 +1,12 @@
-import { useAtomValue } from "jotai";
 import {
   optionsEnviroments,
   useConfiguration,
 } from "../hooks/useConfiguration";
-import TOOL_ATOM from "../states/tool";
 import { InputSelect } from "./input-select";
 
 export const StageMode = () => {
   const { config, change } = useConfiguration();
-  const tool = useAtomValue(TOOL_ATOM);
 
-  if (tool !== "MOVE") return null;
   return (
     <InputSelect
       labelText="Mode"
