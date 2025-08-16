4460177e0e841ea88ecb30a06dffcaeb5699db1b
ivanglpz <miltonivan117@gmail.com>
2025-08-15 20:51:49 -0600

    update sidebar with styles and chat mode

--- src/editor/sidebar/sidebar.left.tsx
+++ src/editor/sidebar/sidebar.left.tsx
@@ -1,7 +1,5 @@
 import { css } from "@stylespixelkit/css";
 import { FC } from "react";
-import { Chat } from "../components/chat";
-import { Segmentation } from "../components/segmentation";
 import { SidebarLeftPages } from "./left/pages";
 import { SidebarLeftShapes } from "./left/shapes";
 
@@ -17,36 +15,36 @@ export const SidebarLeft: FC = () => {
         overflow: "hidden",
       })}
     >
-      <Segmentation>
-        <Segmentation.Item id="1" title="Layers">
-          <div
-            className={css({
-              display: "grid",
-              gridTemplateRows: "240px 1px 1fr",
-              overflow: "hidden",
-              height: "100%",
-              gap: "lg",
-            })}
-          >
-            <SidebarLeftPages />
-            <div
-              className={css({
-                backgroundColor: "gray.500",
-                width: "100%",
-                height: 1,
-                // paddingBottom: "lg",
-              })}
-            ></div>
-            <SidebarLeftShapes />
-          </div>
-        </Segmentation.Item>
-        {/* <Segmentation.Item id="2" title="Assets">
+      {/* <Segmentation>
+        <Segmentation.Item id="1" title="Layers"> */}
+      <div
+        className={css({
+          display: "grid",
+          gridTemplateRows: "240px 1px 1fr",
+          overflow: "hidden",
+          height: "100%",
+          gap: "lg",
+        })}
+      >
+        <SidebarLeftPages />
+        <div
+          className={css({
+            backgroundColor: "gray.500",
+            width: "100%",
+            height: 1,
+            // paddingBottom: "lg",
+          })}
+        ></div>
+        <SidebarLeftShapes />
+      </div>
+      {/* </Segmentation.Item> */}
+      {/* <Segmentation.Item id="2" title="Assets">
           <p>Contenido 2</p>
         </Segmentation.Item> */}
-        <Segmentation.Item id="2" title="Chat">
+      {/* <Segmentation.Item id="2" title="Chat">
           <Chat />
         </Segmentation.Item>
-      </Segmentation>
+      </Segmentation> */}
     </aside>
   );
 };
