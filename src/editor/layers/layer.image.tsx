fc75b7623a2825237e0def377398bd62812905bc
ivanglpz <miltonivan117@gmail.com>
2025-08-21 10:53:49 -0600

    update layout layer with preview types

--- src/editor/layers/layer.image.tsx
+++ src/editor/layers/layer.image.tsx
@@ -7,6 +7,7 @@ import { ShapeImage } from "../shapes/image.shape";
 import { IShape } from "../shapes/type.shape";
 import { STAGE_DIMENSION_ATOM } from "../states/dimension";
 import { IMAGE_RENDER_ATOM } from "../states/image";
+import { ALL_SHAPES } from "../states/shapes";
 
 export const LayerImage = () => {
   const image = useAtomValue(IMAGE_RENDER_ATOM);
@@ -28,40 +29,74 @@ export const LayerImage = () => {
   )
     return null;
 
+  const shape: IShape = {
+    ...dimension,
+    align: "center",
+    tool: "IMAGE",
+    visible: true,
+    isLocked: true,
+    dash: 0,
+    rotation: 0,
+    isCreating: true,
+    fills: [
+      {
+        color: "#fff",
+        id: "107183aa-dbb0-4986-9d17-066e30e88996",
+        image: {
+          src: image?.base64,
+          height: image.height,
+          width: image.width,
+          name: "preview-edit-image",
+        },
+        opacity: 1,
+        type: "image",
+        visible: true,
+      },
+    ],
+    id: "main-image-render-stage",
+    verticalAlign: "top",
+    bordersRadius: [0, 0, 0, 0],
+    effects: [],
+    label: "DRAW",
+    parentId: null,
+    opacity: 1,
+    strokes: [
+      {
+        id: "559c1735-4e62-4c43-aa4c-246ec594ca06",
+        visible: true,
+        color: "#fff",
+      },
+    ],
+    points: [],
+    strokeWidth: 1,
+    backgroundColor: "#ffffff",
+    lineCap: "round",
+    lineJoin: "round",
+    shadowBlur: 0,
+    shadowOffsetY: 5,
+    shadowOffsetX: 5,
+    shadowOpacity: 1,
+    isAllBorderRadius: false,
+    borderRadius: 0,
+    fontStyle: "Roboto",
+    textDecoration: "none",
+    zIndex: 0,
+    fontWeight: "normal",
+    fontFamily: "Roboto",
+    fontSize: 24,
+    text: "",
+  };
+  const state: ALL_SHAPES = {
+    id: "1c46a759-c0f4-4978-8781-5f10ca8cfe8d",
+    parentId: null,
+    pageId: "f860ad7b-27ac-491a-ba77-1a81f004dac1",
+    state: atom(shape),
+    tool: "IMAGE",
+  };
+
   return (
     <Layer id="layer-image-preview">
-      <ShapeImage
-        listShapes={[]}
-        shape={{
-          id: "1c46a759-c0f4-4978-8781-5f10ca8cfe8d",
-          parentId: null,
-          pageId: "f860ad7b-27ac-491a-ba77-1a81f004dac1",
-          state: atom({
-            ...dimension,
-            id: "main-image-render-stage",
-            tool: "IMAGE",
-            visible: true,
-            isLocked: true,
-            dash: 0,
-            fills: [
-              {
-                color: "#fff",
-                id: "107183aa-dbb0-4986-9d17-066e30e88996",
-                image: {
-                  src: image?.base64,
-                  height: image.height,
-                  width: image.width,
-                  name: "preview-edit-image",
-                },
-                opacity: 1,
-                type: "image",
-                visible: true,
-              },
-            ],
-          } as IShape),
-          tool: "IMAGE",
-        }}
-      />
+      <ShapeImage listShapes={[]} shape={state} />
     </Layer>
   );
 };
