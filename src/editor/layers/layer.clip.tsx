c5d5bddf8c922ef996efb0e585b562ff1610b51f
ivanglpz <miltonivan117@gmail.com>
2025-08-21 11:03:55 -0600

    fix clip image

--- src/editor/layers/layer.clip.tsx
+++ src/editor/layers/layer.clip.tsx
@@ -15,6 +15,7 @@ import {
 } from "../states/clipImage";
 import { STAGE_DIMENSION_ATOM } from "../states/dimension";
 import { IMAGE_RENDER_ATOM } from "../states/image";
+import { ALL_SHAPES } from "../states/shapes";
 
 export const LayerClip = () => {
   // const { img } = useImageRender();
@@ -82,6 +83,71 @@ export const LayerClip = () => {
 
   if (!showClip) return null;
 
+  const shape: IShape = {
+    ...dimension,
+    align: "center",
+    tool: "IMAGE",
+    visible: true,
+    isLocked: true,
+    dash: 0,
+    rotation: 0,
+    isCreating: true,
+    fills: [
+      {
+        color: "#fff",
+        id: "107183aa-dbb0-4986-9d17-066e30e88996",
+        image: {
+          src: image?.base64,
+          height: image.height,
+          width: image.width,
+          name: "preview-edit-image",
+        },
+        opacity: 1,
+        type: "image",
+        visible: true,
+      },
+    ],
+    id: "main-image-render-stage",
+    verticalAlign: "top",
+    bordersRadius: [0, 0, 0, 0],
+    effects: [],
+    label: "DRAW",
+    parentId: null,
+    opacity: 1,
+    strokes: [
+      {
+        id: "559c1735-4e62-4c43-aa4c-246ec594ca06",
+        visible: true,
+        color: "#fff",
+      },
+    ],
+    points: [],
+    strokeWidth: 1,
+    backgroundColor: "#ffffff",
+    lineCap: "round",
+    lineJoin: "round",
+    shadowBlur: 0,
+    shadowOffsetY: 5,
+    shadowOffsetX: 5,
+    shadowOpacity: 1,
+    isAllBorderRadius: false,
+    borderRadius: 0,
+    fontStyle: "Roboto",
+    textDecoration: "none",
+    zIndex: 0,
+    fontWeight: "normal",
+    fontFamily: "Roboto",
+    fontSize: 24,
+    text: "",
+  };
+  const state: ALL_SHAPES = {
+    id: "1c46a759-c0f4-4978-8781-5f10ca8cfe8d",
+    parentId: null,
+    pageId: "f860ad7b-27ac-491a-ba77-1a81f004dac1",
+    state: atom(shape),
+    tool: "IMAGE",
+  };
+
   return (
     <Layer id="layer-clip-image-preview">
       <Rect
@@ -98,38 +164,7 @@ export const LayerClip = () => {
         clipX={box.x}
         clipY={box.y}
       >
-        <ShapeImage
-          listShapes={[]}
-          shape={{
-            id: "d5644580-df24-48ad-87fa-5cb4a51a5983",
-            pageId: "main-image-render-stage",
-            parentId: null,
-            state: atom({
-              ...dimension,
-              id: "main-image-render-stage",
-              tool: "IMAGE",
-              visible: true,
-              dash: 0,
-              isLocked: true,
-              fills: [
-                {
-                  color: "#fff",
-                  id: "b45ef6ff-fd0b-4dcc-9be1-68a912723d14",
-                  image: {
-                    src: image?.base64,
-                    height: image.height,
-                    width: image.width,
-                    name: "preview-edit-image",
-                  },
-                  opacity: 1,
-                  type: "image",
-                  visible: true,
-                },
-              ],
-            } as IShape),
-            tool: "IMAGE",
-          }}
-        />
+        <ShapeImage listShapes={[]} shape={state} />
       </Group>
       <Rect
         ref={shapeRef as MutableRefObject<Konva.Rect>}
