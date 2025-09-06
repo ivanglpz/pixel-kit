09c13f30d9c271f66afc8be907b5596fa0ebf022
ivanglpz <miltonivan117@gmail.com>
2025-09-06 16:06:52 -0600

    update font family options

--- src/editor/sidebar/sidebar-right-shape.tsx
+++ src/editor/sidebar/sidebar-right-shape.tsx
@@ -301,8 +301,37 @@ export const LayoutShapeConfig = () => {
 
   // Opciones para selects
   const fontFamilyOptions = [
-    { id: "font-Roboto", label: "Roboto", value: "Roboto" },
     { id: "font-Arial", label: "Arial", value: "Arial" },
+    { id: "font-ArialBlack", label: "Arial Black", value: "Arial Black" },
+    { id: "font-Verdana", label: "Verdana", value: "Verdana" },
+    { id: "font-Tahoma", label: "Tahoma", value: "Tahoma" },
+    { id: "font-TrebuchetMS", label: "Trebuchet MS", value: "Trebuchet MS" },
+    { id: "font-Impact", label: "Impact", value: "Impact" },
+    {
+      id: "font-TimesNewRoman",
+      label: "Times New Roman",
+      value: "Times New Roman",
+    },
+    { id: "font-Georgia", label: "Georgia", value: "Georgia" },
+    { id: "font-Palatino", label: "Palatino", value: "Palatino" },
+    { id: "font-Garamond", label: "Garamond", value: "Garamond" },
+    { id: "font-CourierNew", label: "Courier New", value: "Courier New" },
+    {
+      id: "font-LucidaConsole",
+      label: "Lucida Console",
+      value: "Lucida Console",
+    },
+    {
+      id: "font-LucidaSansUnicode",
+      label: "Lucida Sans Unicode",
+      value: "Lucida Sans Unicode",
+    },
+    { id: "font-ComicSansMS", label: "Comic Sans MS", value: "Comic Sans MS" },
+    { id: "font-Candara", label: "Candara", value: "Candara" },
+    { id: "font-Calibri", label: "Calibri", value: "Calibri" },
+    { id: "font-Cambria", label: "Cambria", value: "Cambria" },
+    { id: "font-Constantia", label: "Constantia", value: "Constantia" },
+    { id: "font-Consolas", label: "Consolas", value: "Consolas" },
   ];
 
   const fontWeightOptions = [
