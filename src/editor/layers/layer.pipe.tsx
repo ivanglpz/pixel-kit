fd388e58f472399c1b7057295e981c85771304f0
ivanglpz <miltonivan117@gmail.com>
2025-09-10 16:23:12 -0600

    fix validate pipe children

--- src/editor/layers/layer.pipe.tsx
+++ src/editor/layers/layer.pipe.tsx
@@ -2,6 +2,7 @@ import { atom, useAtomValue } from "jotai";
 import { Shapes } from "../shapes/shapes";
 import { FCShapeWEvents } from "../shapes/type.shape";
 import CURRENT_ITEM_ATOM from "../states/currentItem";
+import { ALL_SHAPES } from "../states/shapes";
 
 export const LayerPipe = () => {
   const CURRENT_ITEMS = useAtomValue(CURRENT_ITEM_ATOM);
@@ -16,7 +17,12 @@ export const LayerPipe = () => {
           <Component
             shape={{
               id: "1",
-              state: atom(item),
+              state: atom({
+                ...item,
+                children: item?.children
+                  ? item.children
+                  : atom<ALL_SHAPES[]>([]),
+              }),
               pageId: "main-image-render-stage",
               tool: item.tool,
             }}
