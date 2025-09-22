d65ff1dc40f6e1cbc552a9de0c0ae43fba947b5f
ivanglpz <miltonivan117@gmail.com>
2025-09-21 18:40:34 -0600

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+const { FlatCompat } = require("@eslint/eslintrc");
+const { dirname } = require("path");
+const { fileURLToPath } = require("url");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,7 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+module.exports = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
@@ -21,5 +21,3 @@ const eslintConfig = [
     ],
   },
 ];
-
-export default eslintConfig;
