bfb2caa389f39506222a76262053afbff3736e6a
ivanglpz <miltonivan117@gmail.com>
2025-09-23 21:43:01 -0600

    update types with endpoint

---
+++ src/db/schemas/types.ts
@@ -0,0 +1,29 @@
+export type Role = "admin" | "member";
+
+export type RoleProject = "developer" | "designer" | "viewer" | "commenter";
+
+export type IUser = {
+  email: string;
+  fullName: string;
+  photoUrl: string;
+};
+
+export type IMembers<role> = {
+  user: IUser;
+  role: role;
+};
+
+export type IOrganization = {
+  name: string;
+  members: IMembers<Role>[];
+  projects: [];
+  owner: IUser;
+};
+
+export type IProject = {
+  name: string;
+  organization: IOrganization;
+  createdBy: IUser;
+  data: string;
+  members: IMembers<RoleProject>[];
+};
