60797eeba21d016c60ea27573d1663f6eec450eb
ivanglpz <miltonivan117@gmail.com>
2025-10-07 12:17:35 -0600

    stable version

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ if (
 
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
