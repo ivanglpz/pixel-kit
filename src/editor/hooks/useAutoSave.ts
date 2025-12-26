0cb0ad86e94cbcc563d266a380ddf38bfa878e0c
ivanglpz <miltonivan117@gmail.com>
2025-12-26 09:12:45 -0600

    refactor: enhance photo upload and preview functionality with type integration and improved API handling

--- src/editor/hooks/useAutoSave.ts
+++ src/editor/hooks/useAutoSave.ts
@@ -1,4 +1,5 @@
 import { IProject } from "@/db/schemas/types";
+import { uploadPhotoPreview } from "@/services/photo";
 import { updateProject } from "@/services/projects";
 import { useMutation } from "@tanstack/react-query";
 import { useSetAtom } from "jotai";
@@ -6,6 +7,17 @@ import { toast } from "sonner";
 import { GET_EXPORT_ALLSHAPES_ATOM } from "../states/mode";
 import { GET_JSON_PROJECTS_ATOM } from "../states/projects";
 import { useDelayedExecutor } from "./useDelayExecutor";
+
+function base64ToFile(base64: string, fileName: string): File {
+  const [header, data] = base64.split(",");
+  const mimeMatch = header.match(/:(.*?);/);
+  const mimeType = mimeMatch ? mimeMatch[1] : "image/png";
+
+  const binary = atob(data);
+  const array = Uint8Array.from(binary, (c) => c.charCodeAt(0));
+
+  return new File([array], fileName, { type: mimeType });
+}
 
 export const useAutoSave = () => {
   const GET_JSON = useSetAtom(GET_JSON_PROJECTS_ATOM);
@@ -15,12 +27,17 @@ export const useAutoSave = () => {
     mutationFn: async () => {
       const JSON_ = GET_JSON();
       const PREVIEW = await GET_PREVIEW();
+      const formData = new FormData();
+      formData.append("image", base64ToFile(PREVIEW, "preview.png")); // usar el mismo nombre 'images'
+      formData.append("projectId", `${JSON_.projectId}`); // usar el mismo nombre 'images'
+
+      const response = await uploadPhotoPreview(formData);
 
       const PAYLOAD: Pick<IProject, "_id" | "name" | "previewUrl" | "data"> = {
         _id: JSON_.projectId,
         data: JSON_.data,
         name: JSON_.projectName,
-        previewUrl: PREVIEW ?? "./placeholder.svg",
+        previewUrl: response?.url ?? "./placeholder.svg",
       };
       updateProject(PAYLOAD);
     },
@@ -33,7 +50,7 @@ export const useAutoSave = () => {
     callback: () => {
       mutation.mutate();
     },
-    timer: 6000, // opcional
+    timer: 5000, // opcional
   });
   return {
     debounce,
