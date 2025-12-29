2e0b086cf304746290f140b09e2c19102304c56b
ivanglpz <miltonivan117@gmail.com>
2025-12-28 23:16:36 -0600

    remove react scan

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          crossOrigin="anonymous"
-          src="//unpkg.com/react-scan/dist/auto.global.js"
-        />
-        {/* rest of your scripts go under */}
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
