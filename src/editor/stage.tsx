ce057d68175d7e2a4c219be99c7a5c19b15a3666
ivanglpz <miltonivan117@gmail.com>
2025-12-30 19:40:03 -0600

    removed chat tool stage

--- src/editor/stage.tsx
+++ src/editor/stage.tsx
@@ -5,7 +5,6 @@ import Konva from "konva";
 import { KonvaEventObject } from "konva/lib/Node";
 import { ReactNode, useEffect, useRef, useState } from "react";
 import { Stage } from "react-konva";
-import { ChatTool } from "./components/chat";
 import { cursor_event, STAGE_IDS } from "./constants/stage";
 import { useEventStage } from "./hooks/useEventStage";
 import { Tools } from "./sidebar/Tools";
@@ -154,7 +153,6 @@ const PxStage = ({ children }: { children: ReactNode }) => {
         >
           {children}
         </Stage>
-        <ChatTool />
       </div>
     </section>
   );
